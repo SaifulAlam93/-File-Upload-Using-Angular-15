d3c178fc12d5ae294994dd67bc2f296ac4d2781d
G M Saiful Alam <59245133+SaifulAlam93@users.noreply.github.com>
2023-04-17 02:20:06 +0600

    first

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('image', file);
+
+    const req = new HttpRequest('POST', `${this.baseUrl}/image/fileSystem`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/image/fileSystem`);
+  }
+}
